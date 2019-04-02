9f191edbe92d4c4a6d71b8d45a6ad9941023b89f
annasylvester <annaleesylvester@gmail.com>
2019-04-02 18:37:45 -0400

    Everything but comments working

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -13,21 +13,21 @@ $(document).ready(function() {
 	  for (var i = 0; i < data.length; i++) {
 	    // Display the information on the page
 	    $("#scrape-results").prepend("<div class='result-div'><img class='result-pic' src='" + data[i].imageURL + "'><div class='result-text'><div class='article-title'>" +data[i].title + "</div><br><div class='artcle-description'>" + data[i].description +
-	    	"</div></div><button class='save-article button is-success is-medium' data-id='" + data[i]._id + "'><span class='icon'><i class='fa fa-map-pin'></i></span>Pin Article</button></div></div><br>");
+	    	"</div></div><button class='pin-article button is-success is-medium' data-id='" + data[i]._id + "'><span class='icon'><i class='fa fa-map-pin'></i></span>Pin Article</button></div></div><br>");
 	  }
 	});
 
 	// Save article button changes the saved property of the article model from false to true
-	$(document).on("click", ".save-article", function() {
+	$(document).on("click", ".pin-article", function() {
 		// change icon to check mark
-		$(this).children("span.icon").children("i.fa-bookmark").removeClass("fa-bookmark").addClass("fa-check-circle");
+		$(this).children("span.icon").children("i.fa-map-pin").removeClass("fa-map-pin").addClass("fa-check-circle");
 		// Get article id
 		var articleID = $(this).attr("data-id");
 		console.log(articleID);
 		// Run a POST request to update the article to be saved
 	  $.ajax({
 	    method: "POST",
-	    url: "/save/" + articleID,
+	    url: "/pinned/" + articleID,
 	    data: {
 	      saved: true
 	    }
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const cheerio = require("cheerio");
 const axios = require("axios");
-// const db = require("../models")
-// var Comment = require("../models/comment.js");
-var Article = require("../models/articles");
+var Comments = require("../models/comments");
+const Article = require("../models/articles");
 
+
+///////// HOME PAGE /////////
 router.get("/all", function (req, res) {
     // Find all results from the scrapedData collection in the db
     Article.find({}, function (error, found) {
@@ -39,10 +40,11 @@ router.get("/scrape", function (req, res) {
             article.description = $(element).children(".content").children(".field-abstract").text();
             // Save the image of each element
             article.imageURL = $(element).children(".content").children("a").children("img").attr("src");
-            console.log(article.title);
-            console.log(article.link);
-            console.log(article.description)
-            console.log(article.imageURL)
+            // Debugging
+            // console.log(article.title);
+            // console.log(article.link);
+            // console.log(article.description)
+            // console.log(article.imageURL)
 
             Article.updateOne({
                 title: article.title
@@ -56,6 +58,116 @@ router.get("/scrape", function (req, res) {
         });
     });
 });
+
+// This will get the articles we scraped from the mongoDB
+router.get("/articles", function(req, res) {
+    // Grab every doc in the Articles array
+    Article.find({})
+    // Execute the above query
+    .exec(function(err, doc) {
+      // Log any errors
+      if (err) {
+        console.log(error);
+      }
+      // Or send the doc to the browser as a json object
+      else {
+        res.json(doc);
+      }
+    });
+  });
+
+// Pin an article
+router.post("/pinned/:id", function (req, res) {
+    // Use the article id to find and update it's saved property to true
+    Article.updateOne({
+            "_id": req.params.id
+        }, {
+            "saved": true
+        })
+        // Execute the above query
+        .exec(function (err, doc) {
+            // Log any errors
+            if (err) {
+                console.log(err);
+            }
+            // Log result
+            else {
+                console.log("doc: ", doc);
+            }
+        });
+});
+
+
+///////// PINNED PAGE /////////
+// Grab an article by it's ObjectId
+router.get("/articles/:id", function (req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    Article.findOne({
+            "_id": req.params.id
+        })
+        // ..and populate all of the comments associated with it
+        .populate("comments")
+        // now, execute our query
+        .exec(function (error, doc) {
+            // Log any errors
+            if (error) {
+                console.log(error);
+            }
+            // Otherwise, send the doc to the browser as a json object
+            else {
+                res.json(doc);
+            }
+        });
+});
+// // Create a new comment
+// router.post("/comment/:id", function(req, res) {
+//     // Create a new comment and pass the req.body to the entry
+//     var newComment = new Comment(req.body);
+//     // And save the new comment the db
+//     newComment.save(function(error, newComment) {
+//       // Log any errors
+//       if (error) {
+//         console.log(error);
+//       }
+//       // Otherwise
+//       else {
+//         // Use the article id to find and update it's comment
+//         Article.updateOne({ "_id": req.params.id }, { $push: { "comments": newComment._id }}, { new: true })
+//         // Execute the above query
+//         .exec(function(err, doc) {
+//           // Log any errors
+//           if (err) {
+//             console.log(err);
+//           }
+//           else {
+//             console.log("doc: ", doc);
+//             // Or send the document to the browser
+//             res.send(doc);
+//           }
+//         });
+//       }
+//     });
+//   });
+  
+//   // Remove a saved article
+//   router.post("/unsave/:id", function(req, res) {
+//     // Use the article id to find and update it's saved property to false
+//     Article.updateOne({ "_id": req.params.id }, { "saved": false })
+//     // Execute the above query
+//     .exec(function(err, doc) {
+//       // Log any errors
+//       if (err) {
+//         console.log(err);
+//       }
+//       // Log result
+//       else {
+//         console.log("Article Removed");
+//       }
+//     });
+//     res.redirect("/pinned");
+//   });
+  
+
 
 
 module.exports = router;
